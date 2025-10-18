cbdbb9446d5d695beaa03165344dec5238ebf99f
Mayank Agrawal <121036421+LittleCodr@users.noreply.github.com>
2025-10-18 16:53:37 +0530

    Create findSmallestInteger.java

---
+++ findSmallestInteger.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int findSmallestInteger(int[] nums, int value) {
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        // Count occurrences of each remainder
+        for (int num : nums) {
+            int rem = ((num % value) + value) % value;
+            freq.put(rem, freq.getOrDefault(rem, 0) + 1);
+        }
+
+        int mex = 0;
+        while (true) {
+            int rem = mex % value;
+            if (!freq.containsKey(rem) || freq.get(rem) == 0) {
+                return mex;
+            }
+            freq.put(rem, freq.get(rem) - 1);
+            mex++;
+        }
+    }
+}
